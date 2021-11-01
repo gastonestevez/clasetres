1ba46620b494d101cfdca7fd785b4d0539ad33e1
Gastón Estévez <gaston@digitalhouse.edu.ar>
2021-11-01 19:49:25 -0300

    agrego componente de usuario

---
+++ users.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const users = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+users.propTypes = {
+
+}
+
+export default users
