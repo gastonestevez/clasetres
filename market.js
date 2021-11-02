8b4f3be1e1577b5c376d14889d9b149c52ab9ebc
Gastón Estévez <gaston@digitalhouse.edu.ar>
2021-11-01 21:02:20 -0300

    agrego cosas a componente market

--- market.js
+++ market.js
@@ -3,7 +3,8 @@ import React from 'react'
 export const market = () => {
     return (
         <div>
-            
+            Este es mi componente de market.
+            Agrego mas cosas
         </div>
     )
 }
